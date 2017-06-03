79ff47b192b5fd5c9e6043df7443bdedbaa56adf
etorovsky <etorovsky@users.noreply.github.com>
2017-06-04 02:34:55 +0900

    Format SampleController.java

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -11,31 +11,30 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 
-
 @Controller
 @RequestMapping("/")
 public class SampleController {
-	  // 引数名 `file` は、ファイルのリクエストパラメータ名と一致させる。
-	  @RequestMapping(value="/upload", method=RequestMethod.POST)
-	  public void handle(
-	    HttpServletResponse response,
-	    @RequestParam MultipartFile file
-	  ){
-	    // ファイルが空の場合は HTTP 400 を返す。
-	    if (file.isEmpty()) {
-	      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-	      return;
-	    }
-	    // アップロードされたファイルを保存。
-	    try {
-	      BufferedInputStream in =
-	        new BufferedInputStream(file.getInputStream());
-	      BufferedOutputStream out =
-	        new BufferedOutputStream(
-	          new FileOutputStream(file.getOriginalFilename()));
-	      FileCopyUtils.copy(in, out);
-	    } catch (IOException e) {
-	      throw new RuntimeException("Error uploading file.", e);
-	    }
-	  }
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	String home() {
+		return "Hello, Spring Boot!";
+	}
+
+	// 引数名 `file` は、ファイルのリクエストパラメータ名と一致させる。
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public void upload(HttpServletResponse response, @RequestParam MultipartFile file) {
+		// ファイルが空の場合は HTTP 400 を返す。
+		if (file.isEmpty()) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+		// アップロードされたファイルを保存。
+		try {
+			BufferedInputStream in = new BufferedInputStream(file.getInputStream());
+			BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file.getOriginalFilename()));
+			FileCopyUtils.copy(in, out);
+		} catch (IOException e) {
+			throw new RuntimeException("Error uploading file.", e);
+		}
+	}
 }
