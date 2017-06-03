ef0288cb76ddc019e84cfc99699da0cfac7e769e
etorovsky <etorovsky@users.noreply.github.com>
2017-06-04 01:51:59 +0900

    Add file-upload Refer: http://web-dev.hatenablog.com/entry/spring-boot/intro/file-upload

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller
+@RequestMapping("/")
+public class SampleController {
+	  // 引数名 `file` は、ファイルのリクエストパラメータ名と一致させる。
+	  @RequestMapping(value="/upload", method=RequestMethod.POST)
+	  public void handle(
+	    HttpServletResponse response,
+	    @RequestParam MultipartFile file
+	  ){
+	    // ファイルが空の場合は HTTP 400 を返す。
+	    if (file.isEmpty()) {
+	      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+	      return;
+	    }
+	    // アップロードされたファイルを保存。
+	    try {
+	      BufferedInputStream in =
+	        new BufferedInputStream(file.getInputStream());
+	      BufferedOutputStream out =
+	        new BufferedOutputStream(
+	          new FileOutputStream(file.getOriginalFilename()));
+	      FileCopyUtils.copy(in, out);
+	    } catch (IOException e) {
+	      throw new RuntimeException("Error uploading file.", e);
+	    }
+	  }
+}
