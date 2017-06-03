2125bdc3e046e4a23f7ca0dc049c2fce852e7af3
etorovsky <etorovsky@users.noreply.github.com>
2017-06-04 02:34:28 +0900

    Add the definition of context to DemoApplicationTests.java Refer: https://stackoverflow.com/questions/42127297/failed-to-load-applicationcontext-when-running-integration-tests

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,10 +3,14 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.controller.SampleController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = SampleController.class)
 public class DemoApplicationTests {
 
 	@Test
