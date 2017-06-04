c0b123548397d75d67179323dbfdacbc5a937e0c
etorovsky <etorovsky@users.noreply.github.com>
2017-06-05 01:11:55 +0900

    Config Spring Security Refer: https://spring.io/guides/gs/securing-web/#initial

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.example.demo.config.Security;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@Import(Security.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
